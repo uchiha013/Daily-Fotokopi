016343c61de2e73e240493a0f493c4a80b8ab181
Manuel Kristo Jaftoran <manuelkristo12@gmail.com>
2023-08-18 15:47:09 +0700

    css

---
+++ models/order.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema
+    
+const pesananSchema = new schema({
+            namafotokopi: {
+                type: String
+            },
+            username:{
+                type: string
+            },
+            layanan: {
+                type: String
+            },
+            jenisfile: {
+                type: String
+            },
+            buktifotopembayaran: {
+                type: String
+            },
+            noAntrian: {
+                type: String
+            },
+            status: {
+                type: String
+            }
+        },{
+            timestamps: true,
+})
+
+
+const Pesanan = mongoose.model('Pesanan', pesananSchema)
+
+module.exports = Pesanan
